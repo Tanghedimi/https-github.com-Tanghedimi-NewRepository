9559981291f1c0aabf158f9630a991ab42e40525
tanghedimi <43011896+Tanghedimi@users.noreply.github.com>
2018-10-17 19:52:44 +0200

    kleine aanpassing

--- Les console git/Program.cs
+++ Les console git/Program.cs
@@ -6,7 +6,7 @@ namespace Les_console_git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }
